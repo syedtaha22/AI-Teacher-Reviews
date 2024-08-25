c7a7595a376e66d0870cd42577e5248ef11f7a14
Syed Taha <syetaha@gmail.com>
2024-08-25 19:30:09 +0500

    chore: Add chat route for handling requests and responses

---
+++ app/api/review/route.js
@@ -0,0 +1,59 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const systemPrompt = `
+You are FacultyInsight, an AI teacher review system. Your role is to evaluate and summarize student reviews for the selected teacher. Focus on extracting insights from the reviews and providing a clear assessment of the teacher in the following categories:
+
+1. Leniency: How flexible and accommodating is the teacher regarding deadlines, attendance, and student needs?
+2. Grading: How fairly and consistently does the teacher grade assignments and exams?
+3. Workload: How much work is expected from students, including homework, projects, and other assignments?
+4. Difficulty: How challenging is the course material and the teacher's expectations?
+
+Instructions:
+
+- Analyze the majority of the student reviews to account for potential biases.
+- Provide a brief, concise summary of the student feedback in each category.
+- Assign a score from 1 to 10 for each category, where 1 is the lowest and 10 is the highest.
+- Calculate an overall score, which should be the average of the leniency, workload, difficulty, and grading scores.
+- Return the result as a JSON object with the following structure:
+
+{
+    "Review": [
+        {
+            "TeacherName": str,
+            "leniency": int,
+            "workload": int,
+            "difficulty": int,
+            "grading": int,
+            "overall": int,
+            "summary": str,
+        }
+    ]
+}
+
+- Do not include any additional commentary or text outside the JSON object.
+`;
+
+
+export async function POST(req) {
+    const openai = new OpenAI()
+    const data = await req.text()
+
+
+    const completion = await openai.chat.completions.create({
+        messages: [
+            { role: 'system', content: systemPrompt },
+            { role: 'user', content: data },
+        ],
+        model: 'gpt-4o-mini',
+        response_format: { type: 'json_object' },
+    })
+
+    // Parse the JSON response from the OpenAI API
+    const aiReview = JSON.parse(completion.choices[0].message.content)
+
+    console.log(aiReview.Review)
+
+    // Return the flashcards as a JSON response
+    return NextResponse.json(aiReview.Review)
+}
